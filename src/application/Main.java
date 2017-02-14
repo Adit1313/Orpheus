5e9b038c694090da708ca161d9a473f749f26549
Adit K <aditkirtani@gmail.com>
2017-02-14 15:54:55 +0530

    Working Fullscreen

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,20 @@
 package application;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
 	public static Stage window;
+	
+	private boolean full = false;
 	
 	@Override
 	public void start(Stage stg) {
@@ -19,7 +25,20 @@ public class Main extends Application {
 			Scene scn = new Scene(root);
 			scn.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
-			window.setFullScreen(true);
+			scn.setOnKeyPressed(new EventHandler<KeyEvent>() {
+				@Override
+				public void handle(KeyEvent evt) {
+					if (evt.getCode() == KeyCode.F) {
+						if (full) {
+							window.setFullScreen(false);
+							full = false;
+						} else {
+							window.setFullScreen(true);
+							full = true;
+						}
+					}
+				}
+			});
 			window.setResizable(false);
 			window.setScene(scn);
 			window.setTitle("Orpheus");
